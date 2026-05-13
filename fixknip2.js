e4039942a56d13c0ce350db05ddd8a7ed645012a
jgs-jeeves <jgs-jeeves@users.noreply.github.com>
2026-05-13 03:33:32 +0000

    chore: fix knip configuration hints

--- fixknip2.js
+++ fixknip2.js
@@ -2,5 +2,5 @@ const {execSync} = require('child_process');
 const path = require('path');
 const opts = {cwd: path.join('D:', 'repos', 'karmaniverous', 'jeeves-server'), encoding: 'utf8', stdio: 'pipe'};
 execSync('git add -A', opts);
-execSync('git commit -m "chore: remove stale temp scripts"', opts);
-console.log('done');
+execSync('git commit --amend --no-edit', opts);
+console.log('amended');
